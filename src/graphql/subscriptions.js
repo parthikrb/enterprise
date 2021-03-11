9c75bb3c6423e4a667bfb71f785484bc226d2afb
Parthiban Baskar <parthi.krb@gmail.com>
2021-03-12 01:25:48 +0530

    added user graphql schema

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,66 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateUser = /* GraphQL */ `
+  subscription OnCreateUser {
+    onCreateUser {
+      id
+      firstname
+      lastname
+      username
+      password
+      email
+      role
+      picture {
+        bucket
+        region
+        key
+      }
+      status
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateUser = /* GraphQL */ `
+  subscription OnUpdateUser {
+    onUpdateUser {
+      id
+      firstname
+      lastname
+      username
+      password
+      email
+      role
+      picture {
+        bucket
+        region
+        key
+      }
+      status
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteUser = /* GraphQL */ `
+  subscription OnDeleteUser {
+    onDeleteUser {
+      id
+      firstname
+      lastname
+      username
+      password
+      email
+      role
+      picture {
+        bucket
+        region
+        key
+      }
+      status
+      createdAt
+      updatedAt
+    }
+  }
+`;
